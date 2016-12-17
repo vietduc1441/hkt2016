76a19fbd3f32364c3f79495ec671b9207f00b704
Duy Hoang <h.nguyen@schhanoi.com>
2016-12-17 15:53:04 +0700

    modify templater

--- script.js
+++ script.js
@@ -5,7 +5,9 @@
         .controller('CVController', CVController);
 
     function CVController($timeout, $q, $log, $scope) {
-        $scope.user_profile = USER;
+        $scope.user_profile = user_profile;
+        $scope.user_works = user_works;
+        $scope.user_educations = user_educations;
         $scope.recomment_selections = [
             { id: "1", title: "Summary", value: "1", img: "a.png" },
             { id: "2", title: "Skills", value: "2", img: "a.png" },
@@ -25,7 +27,9 @@
             { id: "16", title: "Certifications", value: "16", img: "a.png" },
             { id: "17", title: "Interests", value: "17", img: "a.png" },
             { id: "18", title: "Personal Details", value: "18", img: "a.png" },
-            { id: "19", title: "Advice for Contacting", value: "19", img: "a.png" }
+            { id: "19", title: "Advice for Contacting", value: "19", img: "a.png" },
+            { id: "20", title: "Education", value: "20", img: "a.png" },
+            { id: "21", title: "Experience", value: "21", img: "a.png" }
         ];
         $scope.onClickSelection = function () {
             let element = document.getElementById(this.selection.id);
@@ -35,32 +39,17 @@
             }
         }
     }
-
-    var USER = {
-        id: "",
-        picture: "a.png",
-        name: "Test user",
-        email: "",
-        company: "AAAAA",
-        country: "VN",
-        data_of_bird: "",
-        work: [],
-        education: []
-    };
-})();
-
-/*
-{
-    "id":"1274230165962032",
-        "name":"Bùi Việt Đức",
-            "picture":{
-                "data":{
-                    "is_silhouette":false,
-                        "url":"https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/12295398_996619057056479_4809850000840301477_n.jpg?oh=146d6cddffb7c545aca56f701226e626&oe=58F5826F"
-                }
-    },
-    "email":"vietduc1441@gmail.com",
-        "work":[
+    var data = {
+        "id": "1274230165962032",
+        "name": "Bùi Việt Đức",
+        "picture": {
+            "data": {
+                "is_silhouette": false,
+                "url": "https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/12295398_996619057056479_4809850000840301477_n.jpg?oh=146d6cddffb7c545aca56f701226e626&oe=58F5826F"
+            }
+        },
+        "email": "vietduc1441@gmail.com",
+        "work": [
             {
                 "description": "develop cool stuffs",
                 "employer": {
@@ -116,31 +105,69 @@
                 "start_date": "2006-12-31",
                 "id": "1273303859387996"
             }],
-            "education":[
-                {
-                    "school": {
-                        "id": "119033104785186",
-                        "name": "THPT Chuyên Nguyễn Tất Thành - Yên Bái"
-                    },
-                    "type": "High School",
-                    "year": {
-                        "id": "141778012509913",
-                        "name": "2008"
-                    }, "id": "1273304249387957"
-                }, {
-                    "school": {
-                        "id": "111936798819016",
-                        "name": "Foreign Trade University"
-                    },
-                    "type": "College",
-                    "id": "1273308386054210"
-                }, {
-                    "school": {
-                        "id": "264986823512471",
-                        "name": "ĐẠI HỌC BÁCH KHOA HÀ NỘI - HANOI UNIVERSITY OF SCIENCE AND TECHNOLOGY"
-                    },
-                    "type": "College",
-                    "id": "1271349489583433"
-                }]
-}
-*/
+        "education": [
+            {
+                "school": {
+                    "id": "119033104785186",
+                    "name": "THPT Chuyên Nguyễn Tất Thành - Yên Bái"
+                },
+                "type": "High School",
+                "year": {
+                    "id": "141778012509913",
+                    "name": "2008"
+                }, "id": "1273304249387957"
+            }, {
+                "school": {
+                    "id": "111936798819016",
+                    "name": "Foreign Trade University"
+                },
+                "type": "College",
+                "id": "1273308386054210"
+            }, {
+                "school": {
+                    "id": "264986823512471",
+                    "name": "ĐẠI HỌC BÁCH KHOA HÀ NỘI - HANOI UNIVERSITY OF SCIENCE AND TECHNOLOGY"
+                },
+                "type": "College",
+                "id": "1271349489583433"
+            }]
+    }
+
+    function mappingUserProfile(userData) {
+        return {
+            id: userData.id,
+            name: userData.name,
+            picture: userData.picture.data.url,
+            email: userData.email,
+            country: "VN",
+            DOB: "",
+            work: getUserWorks(userData.work),
+            education: getUserEducation(userData.education)
+        }
+    }
+    function getUserWorks(works) {
+        var userWorks = "";
+        for (var i = 0; i < works.length; i++) {
+            userWorks += works[i].employer.name;
+            if (i < works.length - 1) {
+                userWorks += ", "
+            }
+        }
+        return userWorks;
+    }
+    function getUserEducation(educations) {
+        var userEducations = "";
+        for (var i = 0; i < educations.length; i++) {
+            userEducations += educations[i].school.name;
+            if (i < educations.length - 1) {
+                userEducations += ", "
+            }
+        }
+        return userEducations;
+    }
+
+    var user_profile = mappingUserProfile(data);
+    var user_educations = data.education;
+    var user_works = data.work;
+})();
+
